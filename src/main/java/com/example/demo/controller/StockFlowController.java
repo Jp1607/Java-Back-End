68cbfa9404229cdfec49c9cf64cf76e2fa95b17e
Jp1607 <joaopedrosousapinto7@gmail.com>
2024-10-24 17:04:10 -0300

    started implementing stock filter

--- src/main/java/com/example/demo/controller/StockFlowController.java
+++ src/main/java/com/example/demo/controller/StockFlowController.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,12 +23,13 @@ public class StockFlowController {
 
     @GetMapping(value = "", produces = "application/json")
     public ResponseEntity<String> listFlow(@RequestParam(value = "flow", required = false) Flow flow,
-                                           @RequestParam(value = "initialDate", required = false) Date initialDate,
-                                           @RequestParam(value = "finalDate", required = false) Date finalDate,
-                                           @RequestParam(value = "storage", required = false) Long storageCenterId) {
+                                           @RequestParam(value = "storage", required = false) Long storageCenterId,
+                                           @RequestParam(value = "initialDate", required = false) @DateTimeFormat(pattern = "dd/mm/yyyy") Date initialDate,
+                                           @RequestParam(value = "finalDate", required = false) @DateTimeFormat(pattern = "dd/mm/yyyy") Date finalDate) {
 
         try {
             StockFlow stockFlow = new StockFlow();
+            stockFlow.setId(null);
             ObjectMapper mapper = new ObjectMapper();
             if (flow != null) {
                 stockFlow.setFlow(flow);
@@ -37,10 +39,20 @@ public class StockFlowController {
                 storageCenter.setId(storageCenterId);
                 stockFlow.setStorageCenter(storageCenter);
             }
-            ExampleMatcher matcher = ExampleMatcher.matching().withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING).withIgnoreNullValues();
+            ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreNullValues().withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
             Example<StockFlow> example = Example.of(stockFlow, matcher);
-            String body = mapper.writeValueAsString(stockFlowRepository.findAll(example).stream().filter(stockFlow1 -> ));
-            System.out.println("body" + mapper.writeValueAsString(stockFlowRepository.findAll(example)));
+            String body = mapper.writeValueAsString(stockFlowRepository.findAll(example).stream().filter(stockFlow1 -> {
+                if (initialDate != null && finalDate != null && initialDate.compareTo(finalDate) == 0) {
+                    return (stockFlow1.getDate().compareTo(initialDate) == 0);
+                } else if (initialDate != null && finalDate == null) {
+                    return (stockFlow1.getDate().after(initialDate));
+                } else if (initialDate == null && finalDate != null) {
+                    return (stockFlow1.getDate().before(finalDate));
+                } else if (initialDate != null) {
+                    return (stockFlow1.getDate().before(finalDate) && stockFlow1.getDate().after(initialDate));
+                } else
+                    return false;
+            }));
             return ResponseEntity.status(200).body(body);
         } catch (Exception e) {
             e.printStackTrace();
